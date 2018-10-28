1b6d3f61336d809d5fed4c99dad1ac13a332fdee
Ivan Stelmakh <stelmahivan1994@gmail.com>
2018-10-28 14:10:12 +0200

    basic structure and styles were added

--- public/js/index.js
+++ public/js/index.js
@@ -1,4 +1,26 @@
-function sum (a, b) {
-  return a + b;
-}
-module.exports = sum;
+'use strict';
+
+(function (global) {
+  const formHeader = global.getElementById('form-header');
+
+  formHeader.addEventListener('click', ({target} = event) => {
+    const loginBtn = global.getElementById('login-btn');
+    const signupBtn = global.getElementById('signup-btn');
+    const loginBody = global.getElementById('login-form');
+    const signupBody = global.getElementById('signup-form');
+
+    if (target.classList.contains('login')) {
+      loginBtn.classList.add('active');
+      signupBtn.classList.remove('active');
+
+      loginBody.classList.remove('is-hide');
+      signupBody.classList.add('is-hide');
+    } else {
+      loginBtn.classList.remove('active');
+      signupBtn.classList.add('active');
+
+      signupBody.classList.remove('is-hide');
+      loginBody.classList.add('is-hide');
+    }
+  });
+})(document);
